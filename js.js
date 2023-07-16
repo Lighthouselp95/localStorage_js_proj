9e9860686eaaadb9d4530ea19117f3caf5ef98ed
Tran Hai Dang <lighthouselp95@gmail.com>
2023-07-16 11:02:27 +0700

    game_over function

--- js.js
+++ js.js
@@ -1,36 +1,57 @@
-"use strict"
+"use strict";
 if (typeof localStorage.getItem("number") !== "string") {
-    localStorage.setItem("number", 0);
-    
-    console.log(`This variable is string and the var is: ${localStorage.getItem("number")}`);
+  localStorage.setItem("number", 0);
+
+  console.log(
+    `This variable is string and the var is: ${localStorage.getItem("number")}`
+  );
 }
 if (typeof localStorage.getItem("thevar") !== "string") {
-    localStorage.setItem("thevar", 1000);
-   
-    console.log(`This variable is string and the var is: ${localStorage.getItem("thevar")}`);
+  localStorage.setItem("thevar", 1000);
+
+  console.log(
+    `This variable is string and the var is: ${localStorage.getItem("thevar")}`
+  );
 }
 
 const a = document.getElementById("text");
-    a.value = localStorage.getItem("thevar");
-
-const myFunction = () => {
-
+a.value = localStorage.getItem("thevar");
 let thenumber = Number(localStorage.getItem("number"));
 const form = document.querySelector("form");
 
 const btn = document.querySelector("#btn");
+const myFunction = () => {
 
-    // form.addEventListener("click", (e)=>e.preventDefault);
-    thenumber++;
-    localStorage.setItem("number", thenumber);
-    console.log(localStorage.getItem("number"));
-    console.log(a.value);
-    
-    
-    localStorage.setItem("thevar", a.value);
-    
-    console.log(localStorage.getItem("thevar"));
+    thenumber = Number(localStorage.getItem("number"));
+  // form.addEventListener("click", (e)=>e.preventDefault);
+  thenumber++;
+  localStorage.setItem("number", thenumber);
+  console.log(localStorage.getItem("number"));
+  console.log(a.value);
+
+  localStorage.setItem("thevar", a.value);
+
+  console.log(localStorage.getItem("thevar"));
+  if (thenumber >= 10) {
+    document.querySelector(".content").innerHTML = `<p>Game over!</p>
+        <p>Do you wanna restart: </p>
+        <button id="restart_btn">Restart<button/>
+        `;
+    document.querySelector("#restart_btn").addEventListener("click", () => {
+      document.querySelector(
+        ".content"
+      ).innerHTML = `<form onsubmit="myFunction()">
+        <label for="text">
+            Please put input: 
+        </label>
+        <input type="text" name="text-input" id="text"></h4>
+        <input type="submit" value="Gửi" id="btn">
+        </form>`;
+      localStorage.setItem("number", 0);
+    });
+  }
 };
+
 /* const myFunction = () => {
     
 
