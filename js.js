cd739943866484b0f5e5ff4f60480ef25e466ed1
Tran Hai Dang <lighthouselp95@gmail.com>
2023-07-16 13:38:14 +0700

    preventdefault

--- js.js
+++ js.js
@@ -13,17 +13,21 @@ if (typeof localStorage.getItem("thevar") !== "string") {
     `This variable is string and the var is: ${localStorage.getItem("thevar")}`
   );
 }
-
+document.querySelector("#times" ).innerHTML = `Lần: ` + localStorage.getItem("number");
 const a = document.getElementById("text");
 a.value = localStorage.getItem("thevar");
 let thenumber = Number(localStorage.getItem("number"));
-const form = document.querySelector("form");
+const form = document.querySelector("#theform");
 
 const btn = document.querySelector("#btn");
-const myFunction = () => {
+form.addEventListener("click", myFunction())
+myFunction = () =>{
+(e) => {
+    e.preventDefault();}
 
+    event=>event.preventDefault()();
     thenumber = Number(localStorage.getItem("number"));
-  // form.addEventListener("click", (e)=>e.preventDefault);
+  
   thenumber++;
   localStorage.setItem("number", thenumber);
   console.log(localStorage.getItem("number"));
@@ -32,22 +36,25 @@ const myFunction = () => {
   localStorage.setItem("thevar", a.value);
 
   console.log(localStorage.getItem("thevar"));
+
   if (thenumber >= 10) {
     document.querySelector(".content").innerHTML = `<p>Game over!</p>
         <p>Do you wanna restart: </p>
-        <button id="restart_btn">Restart<button/>
+        <button id="restart_btn">Restart</button>
         `;
     document.querySelector("#restart_btn").addEventListener("click", () => {
-      document.querySelector(
-        ".content"
-      ).innerHTML = `<form onsubmit="myFunction()">
+        localStorage.setItem("number", 0);
+        //document.location.reload();
+        document.querySelector(".content" ).innerHTML = 
+        `<form onsubmit="myFunction()">
         <label for="text">
             Please put input: 
         </label>
         <input type="text" name="text-input" id="text"></h4>
         <input type="submit" value="Gửi" id="btn">
-        </form>`;
-      localStorage.setItem("number", 0);
+        <i style="margin: 20px">(Click đến 10!)</i>
+        <p>Lần: ${thenumber}</p>
+        </form>`;    
     });
   }
 };
